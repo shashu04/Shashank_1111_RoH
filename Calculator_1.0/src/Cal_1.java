9739c6575e3084f7fc8dff2598543ee261a00142
shashu04 <83244177+shashu04@users.noreply.github.com>
2021-05-05 21:42:00 +0530

    Add files via upload

--- Calculator_1.0/src/Cal_1.java
+++ Calculator_1.0/src/Cal_1.java
@@ -1,57 +1,88 @@
-import java.util.Iterator;
+import java.lang.String;
 import java.util.Scanner;
 
 public class Cal_1 {
 	
-	static int result;
-	static String key;
-	
-	static int add(int a, int b) {
-		result=a+b;
-		return result;
-	}
-	static int sub(int a, int b) {
-		result=a-b;
-		return result;
-	}
-	static int multiply(int a, int b) {
-		result=a*b;
-		return result;
-	}
-	static int div(int a, int b) {
-		result=a/b;
-		return result;
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method 
-		Scanner readInput = new Scanner(System.in);
-		System.out.println("Helloworld");
-		System.out.println("Please select operation : a. Addition \n" + "b. Substraction \n" + "c. Multiplication\n" + "d. Division\n" );
+	Scanner readInput = new Scanner(System.in);
+	String c;
+	public Cal_1() {
+		
+		System.out.println("Please select option:\na. Addition \n" + "b. Substraction \n" + "c. Multiplication\n" + "d. Division\n" + "e. Square\n" );
 		String key = readInput.next();
-		System.out.println("Enter values ");
-	    int a = readInput.nextInt(); 
-	    int b = readInput.nextInt();
+		double a;
+		double b;
 	    switch (key) {
 		case "a":
+			System.out.println("Enter values ");
+			a = readInput.nextInt(); 
+			b = readInput.nextInt();
 			add(a, b);
 			System.out.println(a + " + " + b + " = " + result);
 			break;
 		case "b" :
+			System.out.println("Enter values ");
+			a = readInput.nextInt(); 
+			b = readInput.nextInt();
 			sub(a, b);
 			System.out.println(a + " - " + b + " = " + result);
 		case "c" :
+			System.out.println("Enter values ");
+			a = readInput.nextInt(); 
+			b = readInput.nextInt();
 			multiply(a, b);
 			System.out.println(a + " * " + b + " = " + result);
 			break;
 		case "d" :
+			System.out.println("Enter values ");
+			a = readInput.nextInt(); 
+			b = readInput.nextInt();
 			div(a, b);
 			System.out.println(a + " / " + b + " = " + result);
 			break;
-			
+		case "e" :
+			System.out.println("Enter values ");
+			a = readInput.nextDouble(); 
+			square(a);
+			System.out.println(a + "^2 = " + result);
+			break;
 		default:
+			System.out.println("Invalid operation");
 			break;
 		}
+//	    System.out.println("Do you want to continue? Y/N");
+//	    c = readInput.next();
+
+	}
+	static double result;
+	static String key;
+	
+	static double add(double a, double b) {
+		result=a+b;
+		return result;
+	}
+	static double sub(double a, double b) {
+		result=a-b;
+		return result;
+	}
+	static double multiply(double a, double b) {
+		result=a*b;
+		return result;
+	}
+	static double div(double a, double b) {
+		result=a/b;
+		return result;
+	}
+	static double square(double a) {
+		result=a*a;
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		
+	Cal_1 c = new Cal_1();
+			    
 	    
  	}
+
 
 }
